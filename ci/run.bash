959a9a10031a22107fce39e530c24e4db902e390
Pavel Strakhov <ri@idzaaus.org>
2019-09-12 23:31:05 +0300

    ci11

--- ci/run.bash
+++ ci/run.bash
@@ -9,17 +9,40 @@ wget https://raw.githubusercontent.com/rust-qt/ritual/37cc01f27e2525fb9f6d5882f4
 
 pyenv install --list
 pyenv install 3.6.3
-pyenv versions
 pyenv global 3.6.3
 python --version
+
 pip install bs4
 
 mkdir ~/qt
 cd ~/qt
+QT_VERSION=5.13.0
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    python /tmp/install_qt.py 5.13.0 mac_x64 clang_64
+    QT_OS=mac_x64
+    QT_COMPILER=clang_64
+    QT_SUBDIR=$QT_COMPILER
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    python /tmp/install_qt.py 5.13.0 linux_x64 gcc_x64
+    QT_OS=linux_x64
+    QT_COMPILER=gcc_64
+    QT_SUBDIR=$QT_COMPILER
 elif [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
-    python /tmp/install_qt.py 5.13.0 windows_x86 win64_msvc2017_64
+    QT_OS=windows_x86
+    QT_COMPILER=win64_msvc2017_64
+    QT_SUBDIR=msvc2017_64
+fi
+
+python /tmp/install_qt.py $QT_VERSION $QT_OS $QT_COMPILER
+QT_DIR=~/qt/$QT_VERSION/$QT_SUBDIR
+
+export PATH=$QT_DIR/bin:$PATH
+export LD_LIBRARY_PATH=$QT_DIR/lib:$LD_LIBRARY_PATH
+export QT_QPA_PLATFORM_PLUGIN_PATH=$QT_DIR/plugins
+export QML2_IMPORT_PATH=$QT_DIR/qml
+
+cd "$TRAVIS_BUILD_DIR"
+
+if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
+cmd.exe /C '"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" amd64 && cargo build --all-targets -v'
+else
+    cargo build --all-targets -v
 fi
