eabb8b930a7af11ac471a09a5725d08ba969a9c6
Pavel Strakhov <ri@idzaaus.org>
2019-09-12 23:13:32 +0300

    ci7

--- ci/run.bash
+++ ci/run.bash
@@ -7,6 +7,7 @@ export RUST_BACKTRACE=1
 
 wget https://raw.githubusercontent.com/rust-qt/ritual/37cc01f27e2525fb9f6d5882f447089e2ad5d4bf/scripts/install_qt.py -O /tmp/install_qt.py
 
+pyenv install --list
 pyenv install 3.7.4
 pyenv versions
 pyenv global 3.7.4
